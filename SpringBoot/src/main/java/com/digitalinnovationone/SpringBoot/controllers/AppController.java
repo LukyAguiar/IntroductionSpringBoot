f444f0bdce50bdf60d3f469de465eda9850abacc
Lucas Aguiar <aguiar@kali.Argonaut>
2022-11-25 14:36:33 -0300

    Curso Finalizado : Commando Line - Variáveis de Ambiente

--- SpringBoot/src/main/java/com/digitalinnovationone/SpringBoot/controllers/AppController.java
+++ SpringBoot/src/main/java/com/digitalinnovationone/SpringBoot/controllers/AppController.java
@@ -12,8 +12,14 @@ public class AppController {
     @Value("${app.message}")
     private String appMessage;
 
+    @Value("${ENV_DB_URL:NENHUMA}")
+    private String dbEnvironmentVariable;
     @GetMapping("/profilemsg")
     public String getAppMessage(){
         return appMessage;
+    }
+    @GetMapping("/envVariable")
+    public String getDbEnvironmentVariable(){
+        return "A seguinte variável de ambiente foi passada: " + dbEnvironmentVariable;
     }
 }
